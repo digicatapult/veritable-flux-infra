ff3973a58f0dcf2d3debfba71f23973c85f5afd3
David Blane <32327139+dblane-digicatapult@users.noreply.github.com>
2025-06-23 16:24:57 +0100

    Adds Renovate (#34)
    * Adds Renovate
    
    * Correct usage of extends
    
    * fixing loc

---
+++ renovate.config.js
@@ -0,0 +1,18 @@
+module.exports = (config = {}) => {
+  const isSelfHosted = process.env.RENOVATE_SELF_HOSTED === "true";
+
+  if (!isSelfHosted) {
+    console.log("Renovate is disabled when running via GitHub App.");
+    return {
+      enabled: false,
+      onboarding: false,
+    };
+  }
+
+  console.log("Renovate is running in self-hosted mode.");
+
+  return {
+    $schema: "https://docs.renovatebot.com/renovate-schema.json",
+    extends: "github>digicatapult/renovate-config:flux"
+  };
+};
