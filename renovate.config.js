eb23fdc526d06944b315d377e29584daf5d83d63
David Blane <32327139+dblane-digicatapult@users.noreply.github.com>
2025-06-27 12:02:53 +0100

    Use valid in repo config (#74)

--- renovate.config.js
+++ renovate.config.js
@@ -13,8 +13,55 @@ module.exports = (config = {}) => {
 
   return {
     $schema: "https://docs.renovatebot.com/renovate-schema.json",
-    extends: ["github>digicatapult/renovate-config:flux.json"],
     onboarding: false,
-    requireConfig: false
+    requireConfig: false,
+    baseBranches: ["main"],
+    extends: [":timezone(Europe/London)"],
+    prHourlyLimit: 20,
+    prConcurrentLimit: 20,
+    recreateWhen: "always",
+    flux: {
+      managerFilePatterns: ["^clusters/.*\\.ya?ml$"],
+    },
+    packageRules: [
+      {
+        matchManagers: ["flux"],
+        pinDigests: false,
+      },
+      {
+        matchManagers: ["flux"],
+        schedule: ["* 9,10,11,12,13,14,15,16,17 * * *"],
+      },
+      {
+        matchManagers: ["flux"],
+        matchPaths: ["^clusters/kind-cluster/.*\\.ya?ml$"],
+        automerge: true,
+        addLabels: ["automerge", "kind"],
+        separateMajorMinor: true,
+        separateMultipleMajor: true,
+        separateMinorPatch: true,
+        groupName: "flux - kind-cluster",
+      },
+      {
+        matchManagers: ["flux"],
+        matchPaths: ["^clusters/(?!kind-cluster/)[^/]+/.*\\.ya?ml$"],
+        automerge: false,
+        addLabels: ["production"],
+        separateMajorMinor: true,
+        separateMultipleMajor: true,
+        separateMinorPatch: true,
+        groupName: "flux - non-kind-cluster",
+      },
+      {
+        matchManagers: ["github-actions"],
+        labels: ["dependencies", "github-actions"],
+        groupName: "GitHub Actions - updates",
+        matchUpdateTypes: ["major"],
+        matchPackageNames: ["actions/checkout", "actions/cache"],
+        automerge: true,
+        extends: ["schedule:automergeNonOfficeHours"],
+        addLabels: ["automerge"],
+      },
+    ],
   };
 };
